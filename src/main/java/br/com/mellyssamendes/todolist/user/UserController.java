3944d0752b808c0ed97689b25149877a43bfc139
mellyssamnds <mellyssah.mendes@live.com>
2023-10-15 00:36:24 -0300

    refactor: integration with DB and model and controller refactor

--- src/main/java/br/com/mellyssamendes/todolist/user/UserController.java
+++ src/main/java/br/com/mellyssamendes/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.mellyssamendes.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-
 public class UserController {
+    
+    private static final String USER_ALREADY_EXISTS = "User already exists";
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            System.out.println("User alread exists");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User alread exists");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
